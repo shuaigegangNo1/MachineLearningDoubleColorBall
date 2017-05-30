70b2c8cc88329656cbca17eb86f6cf36c1639bc7
黄雪雯 <huangxuewen@huangxuewen.local>
2017-05-30 21:32:13 +0800

    rm unused package

--- DcbOnWeb/com/sgg/dcb/test/TestImportPyInanotherFile.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append(r"/Users/huangxuewen/PycharmProjects")
-
-#TODO: import self defined module
-
-
-
-
--- DcbOnWeb/com/sgg/dcb/util/ConvertUtil.py
+++
@@ -1,8 +0,0 @@
-def convert_int_to_str(num):
-    return "'" + str(num) + "'"
-
-
-if __name__ == "__main__":
-    a = 4
-    b = convert_int_to_str(4)
-    print(b)
